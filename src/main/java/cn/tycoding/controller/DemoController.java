1da43b3333223fb70178e3e7d5edcb879812a309
git <1246626945@qq.com>
2018-12-26 15:24:49 +0800

    添加demo控制

---
+++ src/main/java/cn/tycoding/controller/DemoController.java
@@ -0,0 +1,11 @@
+package cn.tycoding.controller;
+
+/**
+ * @ClassName DemoController
+ * @Description: TODO
+ * @Author: syf
+ * @CreateDate: 2018/12/26  15:21
+ * @Version: 1.0
+ */
+public class DemoController {
+}
